6304daf2250e4801ad82a8615d6f989504c121e3
Mike Hickman <pdxmike@users.noreply.github.com>
2016-12-11 14:46:08 -0800

    refactored user signup logic

--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,23 @@
 var router = require('express').Router();
+var User = require('../models/user');
 
-router.post('/signup', function(req, res, next){
+router.post('/signup', function(req, res, next) {
   var user = new User();
 
   user.profile.name = req.body.name;
-  user.password = req.body.password;
   user.email = req.body.email;
+  user.password = req.body.password;
 
-  user.save(function(err){
-    if (err) return next(err);
-    res.json('Successfully created new user');
+  User.findOne({email: req.body.email}, function (err, existingUser) {
+    if (existingUser) {
+      console.log(req.body.email + 'already exists');
+      res.redirect('/signup');
+    } else {
+      user.save(function (err, user) {
+        if (err) return next(err);
+        res.json('New user has been created');
+      });
+    }
   });
 });
 

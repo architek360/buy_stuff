3b6a00ea565623509f892c06ec79ce95ae8b878f
Mike Hickman <pdxmike@users.noreply.github.com>
2016-12-11 01:03:00 -0800

    added bootstrap, basic template, home and layout.pug

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express     = require('express'),
     morgan      = require('morgan'),
     bodyParser  = require('body-parser'),
     mongoose    = require('mongoose'),
+    pug         = require('pug'),
     port        = (process.env.PORT || 5000);
 var User        = require('./models/user');
 
@@ -13,9 +14,16 @@ mongoose.connect('mongodb://localhost/first_ecommerce', function(err){
   console.log('Successfully connected to database.');
 });
 
+app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'pug');
+
+
+app.get('/', function(req, res){
+  res.render('home');
+});
 
 app.post('/create-user', function(req, res, next){
   var user = new User();

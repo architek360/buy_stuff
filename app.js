ef840d0be0baef685e24dda5753b2f84b1e1b538
Mike Hickman <pdxmike@users.noreply.github.com>
2016-12-10 23:22:03 -0800

    setup database and user schema

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 'use strict';
 
-var express = require('express'),
-    morgan  = require('morgan'),
-    port    = (process.env.PORT || 5000);
+var express     = require('express'),
+    morgan      = require('morgan'),
+    bodyParser  = require('body-parser'),
+    mongoose    = require('mongoose'),
+    port        = (process.env.PORT || 5000);
 
 var app = express();
+mongoose.connect('mongodb://localhost/first_ecommerce', function(err){
+  if (err) throw err;
+  console.log('Successfully connected to database.');
+});
 
 app.use(morgan('dev'));
 

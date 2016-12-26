824ce60c73daa22efff58bd712ae49153453403e
Mike Hickman <pdxmike@users.noreply.github.com>
2016-12-26 14:40:27 -0800

    add products listing page for categories

--- routes/main.js
+++ routes/main.js
@@ -1,7 +1,17 @@
 var router = require('express').Router();
+var Product = require('../models/product');
 
 router.get('/', function(req, res){
   res.render('home');
 });
 
+router.get('/products/:id', function(req, res, next){
+  Product
+    .find({ category: req.params.id })
+    .populate('category')
+    .exec(function(err, products){
+      if (err) return next(err);
+      res.render('category', { products: products });
+    });
+});
 module.exports = router;
